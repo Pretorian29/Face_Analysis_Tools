626865a3d80847d0831ac048ad9683d8ca5bf84a
pretorian29 <astra_dev@outlook.com>
2022-04-25 15:09:07 +0100

    Executor is the code that will capture X,Y coords from images.

---
+++ code/executor.py
@@ -0,0 +1,14 @@
+"""
+Script 1 - Executor
+
+1. Captures de choosen coordinates X,Y from an image.
+2. Returns the corresponding landmark points for further use.
+
+Paola Montoya 2022
+"""
+#------------------------------------------------------------------
+
+
+
+#------------------------------------------------------------------
+__author__ = 'Paola Montoya 2021'
