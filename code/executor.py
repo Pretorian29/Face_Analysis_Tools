a894781a88d97fdb1372c465ae1cd4d6a561dfae
pretorian29 <astra_dev@outlook.com>
2022-04-28 19:30:29 +0100

    Loading image to process

--- code/executor.py
+++ code/executor.py
@@ -7,6 +7,28 @@ Script 1 - Executor
 Paola Montoya 2022
 """
 #------------------------------------------------------------------
+import sys # to access the system
+import cv2
+
+#------------------------------------------------------------------
+# Loading Image
+
+file_path = './images/10405424_1.jpg'
+
+img = cv2.imread(file_path, cv2.IMREAD_ANYCOLOR)
+ 
+while True:
+    cv2.imshow("Image in Process", img)
+    cv2.waitKey(0)
+    sys.exit() # to exit from all the processes
+ 
+cv2.destroyAllWindows() # destroy all windows
+
+
+
+
+#-----------------------------------------------------------------
+
 
 
 
