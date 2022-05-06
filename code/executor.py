fdad90f7638487b8b05c7f00eeb2ee65fdfcb2b8
pretorian29 <astra_dev@outlook.com>
2022-05-06 13:06:16 +0100

    Adding resized feature to the function

--- code/executor.py
+++ code/executor.py
@@ -26,7 +26,7 @@ def click_event(event, x, y, flags, params):
     # checking for left mouse clicks
     if event == cv2.EVENT_LBUTTONDOWN:
         point_num = point_num + 1
-        cv2.circle(img,(x,y),2,(255,0,0),-1) #Displaying Landmark point
+        cv2.circle(resized,(x,y),2,(255,0,0),-1) #Displaying Landmark point
         
         # displaying the coordinates
         # on the Shell
@@ -35,15 +35,15 @@ def click_event(event, x, y, flags, params):
         # displaying the coordinates
         # on the image window
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, str(point_num) +
+        cv2.putText(resized, str(point_num) +
                     str(''), (x,y), font,
                     0.5, (255, 0, 255), 2)
-        cv2.imshow('image', img)
+        cv2.imshow('image', resized)
  
     # checking for right mouse clicks    
     if event==cv2.EVENT_RBUTTONDOWN:
         point_num = point_num + 1
-        cv2.circle(img,(x,y),2,(255,0,255),-1) #Displaying Landmark point
+        cv2.circle(resized,(x,y),2,(255,0,255),-1) #Displaying Landmark point
         # displaying the coordinates
         # on the Shell
         print(x, ' ', y,' ', point_num)
@@ -51,22 +51,32 @@ def click_event(event, x, y, flags, params):
         # displaying the coordinates
         # on the image window
         font = cv2.FONT_HERSHEY_SIMPLEX
-        b = img[y, x, 0]
-        g = img[y, x, 1]
-        r = img[y, x, 2]
-        cv2.putText(img, str(point_num),
-                    (x,y), font, 1,
+        
+        cv2.putText(resized, str(point_num),
+                    (x,y), font, 0.5,
                     (255, 0, 0), 2)
-        cv2.imshow('image', img)
+        cv2.imshow('image', resized)
  
 # driver function
 if __name__=="__main__":
  
     # reading the image
-    img = cv2.imread(file_path, 1)
+    img = cv2.imread(file_path, cv2.IMREAD_UNCHANGED)
+ 
+    print('Original Dimensions : ',img.shape)
+    
+    scale_percent = 220 # percent of original size
+    width = int(img.shape[1] * scale_percent / 100)
+    height = int(img.shape[0] * scale_percent / 100)
+    dim = (width, height)
+  
+    # resize image
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+ 
+    print('Resized Dimensions : ',resized.shape)
  
     # displaying the image
-    cv2.imshow('image', img)
+    cv2.imshow('image', resized)
  
     # setting mouse handler for the image
     # and calling the click_event() function
